65e9723d2d74226b3cd3da1a223dbbf6a1b11cac
Jeff DeWan <jeff@lumenlearning.com>
2024-10-15 14:10:45 -0700

    OP-517: Add deploy pipelines for livepoll and pandoc

---
+++ deploy/terraform/livepoll/_provider.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = var.region
+
+  assume_role {
+    role_arn = "arn:aws:iam::${var.account}:role/ci-role"
+  }
+}
+
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+  }
+  backend "s3" {
+    encrypt        = true
+    bucket         = "lumen-terraform-state"
+    dynamodb_table = "lumen-terraform-state-lock"
+    region         = "us-west-2"
+    role_arn       = "arn:aws:iam::824635284302:role/terraform"
+  }
+}
---
+++ deploy/terraform/livepoll/parameter_store.tf
@@ -0,0 +1,23 @@
+resource "aws_ssm_parameter" "LIVEPOLL_USE_INSECURE_HTTP" {
+  name  = "${local.parameter_store_prefix}/LIVEPOLL_USE_INSECURE_HTTP"
+  type  = "SecureString"
+  value = "true" # initial value only, updates not handled in terraform
+
+  tags = local.common_tags
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "LIVEPOLL_PASSWORD" {
+  name  = "${local.parameter_store_prefix}/LIVEPOLL_PASSWORD"
+  type  = "SecureString"
+  value = "replace_me" # initial value only, updates not handled in terraform
+
+  tags = local.common_tags
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
